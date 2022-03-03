581928e6bbb479a78e1912c1e4c69d9f9ebd1a12
Gheorghe Luta <georgeluta6@gmail.com>
2022-03-03 11:13:36 +0200

    am adaugat custom button

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  CustomButton({
+    Key? key,
+    required this.color,
+    required this.circularCorners,
+    required this.text,
+    required this.fontSize,
+    required this.onPressed,
+  }) : super(key: key);
+  final Color color;
+  final double circularCorners;
+  final String text;
+  final double fontSize;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      color: color,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(circularCorners),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: fontSize),
+        ),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
